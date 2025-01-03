4160e9402ed8e08d93765c89b6ec108ca8908294
sahrishrahman <160232872+sahrishrahman@users.noreply.github.com>
2025-01-02 21:36:47 -0800

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="max-w-4xl mx-auto  py-16 space-y-10 px-4">
+      <div className="text-center">
+        <h2 className="text-4xl font-playfair font-bold text-[#5E3A87] mb-4">About Us</h2>
+        <p className="text-lg text-gray-700 mb-8">
+        Welcome to our beauty blog, your ultimate source for all things beauty. Our goal is to share expert advice, handy tips, and inspiring content to help you embrace your unique style and boost your confidence. Whether you're new to the world of beauty or a seasoned pro, our blog is thoughtfully designed to meet all your beauty interests.        </p>
+        <p className="text-md text-gray-600">
+        Our mission is to uplift confidence and empower everyone to celebrate their natural beauty. We strive to make beauty approachable, enjoyable, and inclusive for all, regardless of background or experience. To us, beauty is not just about appearance—its about feeling comfortable, confident, and radiant in your own skin.
+        </p>
+        <p className="text-md text-gray-600">
+          With a team of passionate writers and beauty experts, we bring you the latest trends in skincare, makeup, and wellness. From product reviews and tutorials to DIY tips and hacks, our content is designed to educate and entertain. We also collaborate with industry professionals to ensure you receive the most up-to-date and accurate information.
+        </p>
+        <p className="text-md text-gray-600">
+          Our blog isn&apos;t just about beauty—it&apos;s about creating a community. We love hearing from our readers and encourage you to share your thoughts, tips, and experiences with us. Together, we can redefine what beauty means and celebrate its diversity and individuality.
+        </p>
+        <p className="text-md text-gray-600">
+          Thank you for being part of our journey. Stay tuned for exciting updates, exclusive content, and much more. Let&apos;s explore the world of beauty together!
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Author.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Author = () => {
+  return (
+    <section className='bg-white '>
+<h1 className='font-playfair text-black text-2xl font-semibold'>SAHRISH RAHMAN</h1>
+    </section>
+  )
+}
+
+export default Author
